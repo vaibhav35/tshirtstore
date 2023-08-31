089137d61f07560f8fa608a9711067f1687b165e
vaibhav35 <46338021+vaibhav35@users.noreply.github.com>
2023-09-01 01:30:36 +0530

    Update homeController.js
    testing cicd

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -20,7 +20,7 @@ exports.homeDummy = async (req, res) => {
 
     res.status(200).json({
       success: true,
-      greeting: "this is another dummy route",
+      greeting: "this is another dummy route edit",
     });
   } catch (error) {
     console.log(error);
